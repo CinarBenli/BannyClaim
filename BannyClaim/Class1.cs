66b0e45d481543004a79745adc8744348b504e4f
Reponse Mod <cinarbenliiphone@hotmail.com>
2022-07-27 23:06:26 +0300

    Proje dosyası ekle.

---
+++ BannyClaim/Class1.cs
@@ -0,0 +1,79 @@
+﻿using Rocket.API;
+using Rocket.Core.Commands;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BannyClaim
+{
+    public class Class1 : RocketPlugin
+    {
+        protected override void Load()
+        {
+            base.Load();
+
+        }
+
+
+
+        [RocketCommand("barikat", "test", "test", Rocket.API.AllowedCaller.Both)]
+        public void Test(IRocketPlayer caller, string[] command)
+        {
+            StartCoroutine(Test());
+        }
+        public IEnumerator<WaitForSeconds> Test()
+        {
+            while (true)
+            {
+                Helpers.DoForeachBarricade((data, drop, asset) =>
+                {
+                    Vector3 position = data.point;
+                    bool shouldGeneratorsRequiredFuel = false;
+
+                    if (Helpers.CheckIsGenerated(position, shouldGeneratorsRequiredFuel))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        BarricadeManager.damage(drop.model.transform, 1, 3, true, CSteamID.Nil, EDamageOrigin.Unknown);
+                    }
+                });
+
+                Helpers.DoForeachStructure((data, asset) =>
+                {
+                    Vector3 position = data.point;
+                    bool shouldGeneratorsRequiredFuel = false;
+                    var structure = StructureManager.FindStructureByRootTransform(asset.model.transform);
+                    var datas = structure.GetServersideData();
+
+                    if (Helpers.CheckIsGenerated(position, shouldGeneratorsRequiredFuel))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        StructureManager.damage(structure.model.transform, structure.model.position, 1, 3, true, CSteamID.Nil, EDamageOrigin.Unknown);
+                    }
+                });
+                yield return new WaitForSeconds(3);
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BannyClaim/Helpers.cs
@@ -0,0 +1,76 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BannyClaim
+{
+    public class Helpers
+    {
+        public static void DoForeachBarricade(Action<BarricadeData, BarricadeDrop, ItemAsset> action)
+        {
+            var bRegions = BarricadeManager.regions.Clone() as BarricadeRegion[,];
+            foreach (var region in bRegions)
+            {
+                var barricades = region.barricades.ToList();
+                var drops = region.drops.ToList();
+
+                for (int i = 0; i < barricades.Count; i++)
+                {
+                    action.Invoke(barricades[i], drops[i], drops[i].asset as ItemAsset);
+                }
+            }
+        }
+
+        public static void DoForeachStructure(Action<StructureData, StructureDrop> action)
+        {
+            var sRegions = StructureManager.regions.Clone() as StructureRegion[,];
+
+            foreach (var region in sRegions)
+            {
+                var structures = region.structures.ToList();
+                var drops = region.drops.ToList();
+
+                for (int i = 0; i < structures.Count; i++)
+                {
+                    action.Invoke(structures[i], drops[i]);
+                }
+            }
+        }
+
+        public static bool CheckIsGenerated(Vector3 pos, bool requireFuel)
+        {
+            List<RegionCoordinate> regions = new List<RegionCoordinate>();
+            Regions.getRegionsInRadius(pos, PowerTool.MAX_POWER_RANGE, regions);
+
+            float sqrRadius = PowerTool.MAX_POWER_RANGE * PowerTool.MAX_POWER_RANGE;
+
+            List<Transform> barricades = new List<Transform>();
+            BarricadeManager.getBarricadesInRadius(pos, sqrRadius, barricades);
+            BarricadeManager.getBarricadesInRadius(pos, sqrRadius, regions, barricades);
+
+            foreach (var transform in barricades)
+            {
+                var generator = transform.GetComponent<InteractableGenerator>();
+                if (generator == null)
+                    continue;
+
+                if ((generator.transform.position - pos).sqrMagnitude < generator.sqrWirerange)
+                {
+                    if (requireFuel && !generator.isPowered || generator.fuel <= 0)
+                    {
+                        continue;
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
